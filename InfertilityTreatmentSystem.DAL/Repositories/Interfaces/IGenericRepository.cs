7d2cb652e4075385559e37026923f3950193a099
ThanNguyen <156517391+ThanNguyenn@users.noreply.github.com>
2025-07-04 16:23:37 +0700

    Add GenericRepo

---
+++ InfertilityTreatmentSystem.DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfertilityTreatmentSystem.DAL.Repositories.Interfaces;
+public interface IGenericRepository<T> : IDisposable where T : class
+{
+    #region Get Methods
+    Task<T> SingleOrDefaultAsync(
+        Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+    Task<TResult> SingleOrDefaultAsync<TResult>(
+        Expression<Func<T, TResult>> selector,
+        Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+    Task<ICollection<T>> GetListAsync(
+        Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+    Task<ICollection<TResult>> GetListAsync<TResult>(
+        Expression<Func<T, TResult>> selector,
+        Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+    Task<int> CountAsync(Expression<Func<T, bool>> predicate = null);
+
+    Task<T> GetByIdAsync(Guid id);
+
+    IQueryable<T> GetQueryable(
+        Expression<Func<T, bool>> predicate = null,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null);
+
+    #endregion
+    #region Insert
+
+    Task InsertAsync(T entity);
+
+    Task InsertRangeAsync(IEnumerable<T> entities);
+
+    #endregion
+
+    #region Update
+
+    void UpdateAsync(T entity);
+
+    void UpdateRange(IEnumerable<T> entities);
+
+    #endregion
+
+    void DeleteAsync(T entity);
+    void DeleteRangeAsync(IEnumerable<T> entities);
+}
---
+++ InfertilityTreatmentSystem.DAL/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfertilityTreatmentSystem.DAL.Repositories.Interfaces;
+public interface IUnitOfWork : IGenericRepositoryFactory, IDisposable
+{
+    Task<TOperation> ProcessInTransactionAsync<TOperation>(Func<Task<TOperation>> operation);
+    Task ProcessInTransactionAsync(Func<Task> operation);
+
+    int Commit();
+
+    Task<int> CommitAsync();
+
+    Task<IDbContextTransaction> BeginTransactionAsync();
+
+
+    Task CommitTransactionAsync(IDbContextTransaction transaction);
+
+    Task RollbackTransactionAsync(IDbContextTransaction transaction);
+}
+
+public interface IUnitOfWork<TContext> : IUnitOfWork where TContext : DbContext
+{
+    TContext Context { get; }
+}
